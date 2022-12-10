575c8edc7874dcb7e6d4831bd4e78b321229813a
andrewpak <andrew7pak@gmail.com>
2022-12-09 22:42:16 -0500

    main server doesn't need to extend thread

--- Server.java
+++ Server.java
@@ -1,7 +1,7 @@
 import java.net.*;
 import java.io.*;
 
-public class Server extends Thread {
+public class Server {
     private InputStreamReader in;
     private BufferedReader br;
     public Server () {
