f862bf0c310476fb3421a9a8b58a098de63a9aa9
andrewpak <andrew7pak@gmail.com>
2022-12-09 22:39:57 -0500

    add StudentClient

---
+++ StudentClient.java
@@ -0,0 +1,14 @@
+public class StudentClient extends Thread {
+
+    public StudentClient () {
+
+    }
+
+    public void run() {
+
+    }
+    public static void main(String[] args) {
+
+    }
+
+}
