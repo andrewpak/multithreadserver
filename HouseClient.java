271cc3a566cd7f3829d48d510a53125e942e22c9
andrewpak <andrew7pak@gmail.com>
2022-12-10 03:42:26 -0500

    add HouseClient file

---
+++ HouseClient.java
@@ -0,0 +1,15 @@
+public class HouseClient extends Thread {
+
+    public HouseClient () {
+
+    }
+
+    public void run() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+    
+}
